ad384e2b436de7640526552eef4a346467944b44
Maks Makuta <makuta.maks.mmc@gmail.com>
2024-02-08 21:11:46 +0200

    Lab 1

---
+++ makuta/code/lab1task3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program { 
+    static void Main(string[] args) {
+        var program = new Program();
+        program.task();
+    }
+
+    void task() {
+        header("Task 3");
+        Console.WriteLine("Введіть число:");
+        string input = Console.ReadLine();
+        string reversedNumber = ReverseNumber(input);
+        Console.WriteLine($"Число, перевернуте навпаки: {reversedNumber}");
+    }
+
+    string ReverseNumber(string number)
+    {
+        char[] charArray = number.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    private void header(string title) {
+        System.Console.Write("================= ");
+        System.Console.Write(title);
+        System.Console.Write(" =================");
+        System.Console.Write("\n");
+    }
+}
---
+++ makuta/code/lab1task4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program { 
+    static void Main(string[] args) {
+        var program = new Program();
+        program.task();
+    }
+
+    void task() {
+        header("Task 3");
+        Console.WriteLine("Введіть число:");
+        string input = Console.ReadLine();
+        int sum = CalculateDigitSum(input);
+        Console.WriteLine($"Сума цифр числа {input}: {sum}");
+    }
+
+    int CalculateDigitSum(string number)
+    {
+        int sum = 0;
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                sum += (int)char.GetNumericValue(digit);
+            }
+        }
+        return sum;
+    }
+
+    private void header(string title) {
+        System.Console.Write("================= ");
+        System.Console.Write(title);
+        System.Console.Write(" =================");
+        System.Console.Write("\n");
+    }
+}
