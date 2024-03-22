170ab493f2d0ab0fc3899e580f4512e95b26f6a5
Maks Makuta <makuta.maks.mmc@gmail.com>
2024-03-22 15:29:10 +0200

    lab 4

---
+++ makuta/code/lab4Task1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using lab4_main;
+
+namespace lab4
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Aspirant aspirant = new Aspirant("Петро", 25, "Київський політехнічний інститут", "Дослідження в галузі штучного інтелекту");
+            Assert.AreEqual(aspirant.Age, 25);
+            Assert.AreEqual(aspirant.Name, "Петро");
+            Assert.AreEqual(aspirant.University, "Київський політехнічний інститут");
+            Assert.AreEqual(aspirant.ResearchTopic, "Дослідження в галузі штучного інтелекту");
+        }
+    }
+}
